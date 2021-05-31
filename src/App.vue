a19b675985a5a2e85d772b04c691bf030e9ff533
Petar Ostojic <pera.ostojic@gmail.com>
2021-05-31 22:49:14 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,209 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 id="ad-copy">
+      Cash in your knowledge <br />with <span id="spatula">SPATULA</span>
+    </h1>
+    <Card v-bind:profile="searchRes"></Card>
+
+    <div class="tab-container">
+      <span class="tab" @click="clickTwitter()">Twitter</span>
+      <span class="tab" @click="clickIg()">TikTok</span>
+    </div>
+    <!-- <transition-group name="side-fade"> -->
+    <input type="text" name="" id="" v-model="username" />
+
+    <div id="twitter" class="flex-container" v-if="!show">
+      <button class="btn" @click="searchTwitter()">
+        Twitter
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="icon icon-tabler icon-tabler-tools-kitchen"
+          width="30"
+          height="30"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="#2c3e50"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <!-- <path stroke="none" d="M0 0h24v24H0z" fill="none" /> -->
+          <path d="M4 3h8l-1 9h-6z" />
+          <path d="M7 18h2v3h-2z" />
+          <line x1="8" y1="12" x2="8" y2="18" />
+        </svg>
+      </button>
+    </div>
+    <div id="instagram" class="flex-container" v-if="show">
+      <!-- <input type="text" name="" id="" /> -->
+      <button class="btn">
+        TikTok
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="icon icon-tabler icon-tabler-tools-kitchen"
+          width="30"
+          height="30"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="#2c3e50"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <!-- <path stroke="none" d="M0 0h24v24H0z" fill="none" /> -->
+          <path d="M4 3h8l-1 9h-6z" />
+          <path d="M7 18h2v3h-2z" />
+          <line x1="8" y1="12" x2="8" y2="18" />
+        </svg>
+      </button>
+    </div>
+
+    <div class="circle"></div>
+    <!-- </transition-group> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import Card from "./components/Card";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Card,
+  },
+  methods: {
+    clickIg() {
+      this.show = true;
+    },
+    clickTwitter() {
+      this.show = false;
+    },
+    async searchTwitter() {
+      const { data } = await axios.get(
+        `http://188.166.85.199:1313/twitter/${this.username}`
+      );
+      console.log(data);
+      this.searchRes = data;
+      this.numFollowers = data["FollowersCount"];
+    },
+  },
+  data() {
+    return {
+      show: false,
+      username: "",
+      numFollowers: 0,
+      searchRes: {},
+    };
+  },
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;600;900&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  font-weight: 200;
+  font-family: "Nunito", sans-serif;
+}
+
+.flex-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.tab {
+  padding: 10px;
+}
+
+#instagram {
+  /* margin-right: 100%; */
+}
+
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+
+#spatula {
+  color: #8d83f7;
+  /* font-weight: 700; */
+}
+
+#ad-copy {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-weight: 600;
+  font-size: 4rem;
+}
+
+.circle {
+  height: 500px;
+  width: 500px;
+  background-color: #f0edfc;
+  border-radius: 50%;
+  z-index: -1;
+  position: fixed;
+  margin-left: -500px;
+  margin-top: -300px;
+}
+
+button {
+  background-color: #83f7c7;
+  border: none;
+  outline: none;
+  padding: 10px;
+  border-radius: 8px;
+  cursor: pointer;
+  color: #373362;
+  font-weight: 600;
+  margin-top: 10px;
+  font-size: 1rem;
+}
+
+.btn {
+  display: flex;
+  /* justify-content: center; */
+  align-items: center;
+  gap: 10px;
+  padding-left: 30px;
+  padding-right: 23px;
+  -webkit-box-shadow: -1px 5px 15px 5px #e3e0f1;
+  box-shadow: -1px 5px 15px 5px #e3e0f1;
+}
+
+.btn:hover {
+  -webkit-box-shadow: -1px 5px 15px 5px #d9d6e6;
+  box-shadow: -1px 5px 15px 5px #d9d6e6;
+  cursor: pointer;
+  transform: scale(1.01);
+}
+
+@media only screen and (max-width: 600px) {
+  .circle {
+    height: 40vh;
+    width: 40vh;
+    top: 50vh;
+    margin-left: -30px;
+  }
+  #ad-copy {
+    font-size: 2.5rem;
+  }
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="card-container">
+    <div class="left">
+      <img
+        class="avatar"
+        :src="
+          'https://avatars.dicebear.com/api/avataaars/' +
+          profile.username +
+          '.svg'
+        "
+        alt="random-avatar"
+      />
+      <p class="username">{{ profile.username }}</p>
+    </div>
+    <div class="right">
+      <p class="stats">Followers: {{ profile.followersCount }}</p>
+      <p class="stats">Posts: {{ profile.postsCount }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: ["profile"],
+};
+</script>
+
+<style>
+.card-container {
+  width: 38%;
+  height: 130px;
+  display: flex;
+  /* border: 1px solid black; */
+  background-color: #83f7c7;
+  border-radius: 9px;
+  -webkit-box-shadow: -1px 5px 15px 5px #e3e0f1;
+  box-shadow: -1px 5px 15px 5px #e3e0f1;
+}
+
+.right,
+.left {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.left {
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+.left p {
+  padding: 0;
+  /* margin: 0; */
+}
+
+.right {
+  justify-content: center;
+}
+
+.card-container:hover {
+  -webkit-box-shadow: -1px 5px 15px 5px #d9d6e6;
+  box-shadow: -1px 5px 15px 5px #d9d6e6;
+  cursor: pointer;
+  transform: scale(1.01);
+}
+
+.avatar {
+  border-radius: 50%;
+  width: 25%;
+  /* margin-left: 10%; */
+}
+
+.username {
+  /* margin-left: 10%; */
+  color: #8d83f7;
+  font-weight: 600;
+  letter-spacing: 2px;
+  font-size: 25px;
+}
+
+.stats {
+  font-size: 20px;
+  font-weight: 400;
+  letter-spacing: 1px;
+}
+</style>
